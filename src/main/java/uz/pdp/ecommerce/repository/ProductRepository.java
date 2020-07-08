79891e28cb2a2c8d376e63a428e283cd4ab38d35
Samandar Akbarov <akbarov.samandar@gmail.com>
2020-07-09 00:39:28 +0500

    Fix issue

---
+++ src/main/java/uz/pdp/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package uz.pdp.ecommerce.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.pdp.ecommerce.entity.Product;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Page<Product> findAllByCreatedBy(UUID createdBy, Pageable pageable);
+
+}
