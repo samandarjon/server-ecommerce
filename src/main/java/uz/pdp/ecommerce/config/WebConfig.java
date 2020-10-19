a3e99b3be7f3e3bac59e06165fe96b16eb1cd964
Samandar Akbarov <akbarov.samandar@gmail.com>
2020-10-19 17:27:56 +0500

    Enable CORS for frontend

--- src/main/java/uz/pdp/ecommerce/config/WebConfig.java
+++ src/main/java/uz/pdp/ecommerce/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://determined-austin-ba7570.netlify.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
     @Override
